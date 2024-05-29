b484cb9364d47bf0b55c7d4a0a8ed403e93ee72d
Andrew Pomerleau <arpomerleau@gmail.com>
2024-05-29 16:02:19 -0400

    Adding placeholder file for authentication details

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO: Create an OAuth app
+const githubClientId = 'MY_GITHUB_CLIENT_ID_HERE';
+const githubClientSecret = 'MY_GITHUB_CLIENT_SECRET_HERE';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
